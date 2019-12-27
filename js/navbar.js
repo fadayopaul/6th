0cfb5d42e57c4acbc7130c7099d7524a401e3571
Olabamipe Taiwo <teeola48@gmail.com>
2019-12-27 17:38:37 +0000

    Navigation

---
+++ js/navbar.js
@@ -0,0 +1,16 @@
+var menuBtn = document.querySelector('.menu-btn');
+var nav = document.querySelector('nav');
+var lineOne = document.querySelector('.menu-btn .line--1');
+var lineTwo = document.querySelector('.menu-btn .line--2');
+var lineThree = document.querySelector('.menu-btn .line--3');
+var link = document.querySelector('.nav-links');
+
+
+menuBtn.addEventListener('click', () => {
+    nav.classList.toggle('nav-open');
+    lineOne.classList.toggle('line-cross');
+    lineTwo.classList.toggle('line-fade-out');
+    lineThree.classList.toggle('line-cross');
+    link.classList.toggle('fade-in');
+})
+
